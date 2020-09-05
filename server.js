9a13e9de2a839cb3960144855738d22ee2e376cc
Ashley Hoang <ashleyhoang_147@yahoo.com>
2020-09-05 12:51:26 -0400

    fixed server.js and changed PORT to 8083

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8082;
+var PORT = process.env.PORT || 8083;
 
 // Requiring our models for syncing
 var db = require("./models");
