e570d298dee52480b647a5096c290cb2de0d6282
Ashley Hoang <ashleyhoang_147@yahoo.com>
2020-09-13 13:17:37 -0400

    Finished all collectors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8082;
+var PORT = process.env.PORT || 8083;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -24,7 +24,7 @@ app.use(express.static("public"));
 // Controller/Routes
 // =============================================================
 require("./routes/routes-html")(app);
-require("./controllers/user_fav_stock_controller.js")(app);
+require("./controllers/controller.js")(app);
 //require("./routes/routes-api")(app);
 
 // Set Handlebars.
