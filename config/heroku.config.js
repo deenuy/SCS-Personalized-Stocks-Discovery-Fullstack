9b034d527eda901d8ac92065da5bda058379a1c1
deenuy <deenuy@gmail.com>
2020-10-31 03:14:09 -0400

    heroku deployment app issue fix

--- config/heroku.config.js
+++ config/heroku.config.js
@@ -1,6 +1,7 @@
 // Important keys here
 // heroku git:remote -a <your_heroku_app_name>
 // heroku config:set <variable_name=value>
+// heroku open --app YOURAPPNAME
 
 module.exports = {
     apiKeyID: process.env.x-rapidapi-host,
